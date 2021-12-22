b7119e9494efff44bef22f16c1f7a0baaaad7fef
Fernando Román García <fernando.roman@upc.edu>
2021-12-22 12:19:22 +0100

    Fix and partial changes

--- packages/wallet-protocol-api/src/ts/api-method.ts
+++ packages/wallet-protocol-api/src/ts/api-method.ts
@@ -2,11 +2,12 @@ import { WalletPaths } from '@i3m/wallet-desktop-openapi/types'
 
 export interface ApiMethod<T> {
   path: string
-  method?: string
+  method: string
   headers?: Record<string, string>
   _nothing?: T
 }
 
 export const GET_IDENTITIES: ApiMethod<WalletPaths.IdentityList.Responses.$200> = {
-  path: 'identities'
+  path: 'identities',
+  method: 'GET'
 }
