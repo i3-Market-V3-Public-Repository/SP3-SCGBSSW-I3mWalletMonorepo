790ecc3dd9e07c6e2ac50f92991772be3ef817eb
Fernando Román García <fernando.roman@upc.edu>
2022-01-14 12:15:37 +0100

    Remove types from repo

--- packages/wallet-protocol-api/types/api-method.d.ts
+++
@@ -1,9 +0,0 @@
-import { WalletPaths } from '@i3m/wallet-desktop-openapi/types';
-export interface ApiMethod<T> {
-    path: string;
-    method: string;
-    headers?: Record<string, string>;
-    _nothing?: T;
-}
-export declare const GET_IDENTITIES: ApiMethod<WalletPaths.IdentityList.Responses.$200>;
-//# sourceMappingURL=api-method.d.ts.map
