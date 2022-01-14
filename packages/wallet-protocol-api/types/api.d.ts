29dfcef983790c4483897d264981622f3959039c
Fernando Román García <fernando.roman@upc.edu>
2022-01-14 12:15:37 +0100

    Remove types from repo

--- packages/wallet-protocol-api/types/api.d.ts
+++
@@ -1,9 +0,0 @@
-import { Session, HttpInitiatorTransport } from '@i3m/wallet-protocol';
-import { WalletPaths } from '@i3m/wallet-desktop-openapi/types';
-export declare class WalletApi {
-    protected session: Session<HttpInitiatorTransport>;
-    constructor(session: Session<HttpInitiatorTransport>);
-    private executeQuery;
-    getIdentites(queryParams?: WalletPaths.IdentityList.QueryParameters): Promise<WalletPaths.IdentityList.Responses.$200>;
-}
-//# sourceMappingURL=api.d.ts.map
