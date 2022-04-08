d66da773632efe7504a6117c08068937c3325a64
Fernando Román García <fernando.roman@upc.edu>
2022-04-08 11:05:28 +0200

    Disable eslint on js

--- packages/wallet-desktop/res/pairing/main.js
+++ packages/wallet-desktop/res/pairing/main.js
@@ -8,11 +8,8 @@
 // phase3: secret verification
 // ultimo kdf pocas iteraciones
 
-const MAP = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-const base58 = {
-  encode: function (B, A) { var d = []; var s = ''; var i; var j; var c; var n; for (i in B) { j = 0, c = B[i]; s += c || s.length ^ i ? '' : 1; while (j in d || c) { n = d[j]; n = n ? n * 256 + c : c; c = n / 58 | 0; d[j] = n % 58; j++ } } while (j--)s += A[d[j]]; return s },
-  decode: function (S, A) { var d = []; var b = []; var i; var j; var c; var n; for (i in S) { j = 0, c = A.indexOf(S[i]); if (c < 0) return undefined; c || b.length ^ i ? i : b.push(0); while (j in d || c) { n = d[j]; n = n ? n * 58 + c : c; c = n >> 8; d[j] = n % 256; j++ } } while (j--)b.push(d[j]); return new Uint8Array(b) }
-}
+// TODO: Disable eslint here (for now)
+/* eslint-disable */
 
 const { WalletProtocol, HttpInitiatorTransport } = walletProtocol
 const { openModal, LocalSessionManager } = walletProtocolUtils
