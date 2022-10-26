824170c46c35f77c4dc3a80ccf239f861e819112
Juanra Dikal <j.hernandez@upc.edu>
2022-10-26 15:18:57 +0200

    updated deps

--- packages/wallet-desktop/src/main/api/routes/async-handler.ts
+++ packages/wallet-desktop/src/main/api/routes/async-handler.ts
@@ -1,6 +1,6 @@
 import { RequestHandler, Request, Response, NextFunction } from 'express'
 
-export const asyncHandler = <A = never, B = never, C = never, D = never, E = never>(
+export const asyncHandler = <A = never, B = never, C = never, D = never, E extends Record<string, any>= never>(
   handler: (req: Request<A, B, C, D, E>, res: Response<B, E>, next: NextFunction) => Promise<void>
 ): RequestHandler<A, B, C, D, E> => (req: Request<A, B, C, D, E>, res: Response<B, E>, next: NextFunction) => {
   handler(req, res, next).catch(next)
