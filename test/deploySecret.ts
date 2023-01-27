e8bd0b6ffa879d4895e6a9d5043ec34ceaf4e405
juanelas <j.hernandez@upc.edu>
2023-01-27 15:12:40 +0100

    Updated deps. Fixed typos

--- test/deploySecret.ts
+++ test/deploySecret.ts
@@ -7,7 +7,7 @@ function algByteLength (alg: EncryptionAlg | HashAlg | SigningAlg): number {
   return Number((alg.match(/\d{3}/) as RegExpMatchArray)[0]) / 8
 }
 
-describe('Testing deploting secrets', function () {
+describe('Testing deploying secrets', function () {
   this.bail() // stop after a test fails
   this.timeout(2000000)
 
