ac9776fd41f46879cf58b1509bb2a97de0ab10f5
Fernando Román García <fernando.roman@upc.edu>
2023-04-19 17:57:31 +0200

    Removed cloud vault settings comment

--- packages/wallet-desktop/src/renderer/windows/main/routes/wallet/settings/metadatas/cloud-vault.tsx
+++ packages/wallet-desktop/src/renderer/windows/main/routes/wallet/settings/metadatas/cloud-vault.tsx
@@ -1,22 +1,18 @@
-import * as React from 'react'
-
-import { Alert } from 'react-bootstrap'
-
 import { DEFAULT_CLOUD_URL, DEFAULT_UPLOAD_DEBOUNCE_TIME, showToastAction } from '@wallet/lib'
 import { MetadataRecord } from '../settings-metadata'
 
 export const cloudVaultMetadata: MetadataRecord = {
   'Cloud Vault': [
-    {
-      type: 'info',
-      description: {
-        message: (
-          <Alert variant='info'>
-            Here I can put a message descriving what the cloud vault is and more bla bla things.
-          </Alert>
-        )
-      }
-    },
+    // {
+    //   type: 'info',
+    //   description: {
+    //     message: (
+    //       <Alert variant='info'>
+    //         Here I can put a message descriving what the cloud vault is and more bla bla things.
+    //       </Alert>
+    //     )
+    //   }
+    // },
     {
       type: 'input',
       label: 'Cloud URL',
